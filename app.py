46a919948bc6a58fb73a8b32269d4a7f39fac3ee
Pauline Li <pli@teloslogistics.local>
2020-05-12 23:29:30 -0500

    Add app.py updates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 import datetime as dt 
-from sqlalchemy import inspect
+from sqlalchemy import distinct
 #################################################
 # Database Setup
 #################################################
@@ -34,8 +34,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f" /api/v1.0/<start>/<end>"
+        f"/api/v1.0/<start> (Start Date ~ Enter as YYYY-MM-DD)<br/>"
+        f"/api/v1.0/<start>/<end> (Start/End Date ~ Enter as YYYY-MM-DD)"
     )
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -60,8 +60,49 @@ def precipitation():
         precipitaion_dict["precipitation"] = prcp        
         all_prcp.append(precipitaion_dict)
     return jsonify(all_prcp)
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Measurement.station).group_by(Measurement.station).all()
+    #results = session.query(func.count(distinct(Measurement.station))).all()
+    session.close()
+ 
+    unique_stations = list(np.ravel(results))
+    return jsonify(unique_stations)
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    """Return a list of all passenger names"""
+    # Query all passengers
+    Max_Date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date  
+    Twelve_Months = dt.datetime.strptime(Max_Date, '%Y-%m-%d') - dt.timedelta(days=365)
+    
+    
+    # results = session.query(Measurement.station).filter(Measurement.date >= Twelve_Months)\
+    #               .order_by(Measurement.date.desc()).first()
+    stationCount = session.query( Measurement.station, Measurement.date, Measurement.tobs).\
+                    filter(Measurement.date >= Twelve_Months).\
+                    filter(Measurement.station == 'USC00519281').\
+                    order_by(Measurement.date.desc()).all()
 
 
+    #results = session.query(func.count(distinct(Measurement.station))).all()
+    session.close()
+    
+    all_tobs = []
+    for station, date, tobs in stationCount:
+        tobs_dict = {}
+        tobs_dict["station"] = station 
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        all_tobs.append(tobs_dict)
+    return jsonify(all_tobs)
 if __name__ == '__main__':
     app.run(debug=True)
 
+# Query the dates and temperature observations of the most active station for the last year of data.
+# Return a JSON list of temperature observations (TOBS) for the previous year.
